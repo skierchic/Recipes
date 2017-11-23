0e65bbda216f248240daa804d4fd1534847f3fc6
Jenna Hight <jennamhight@gmail.com>
2017-11-22 22:04:20 -0500

    Fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171122035747) do
+ActiveRecord::Schema.define(version: 20171123024332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,11 +19,10 @@ ActiveRecord::Schema.define(version: 20171122035747) do
     t.string "name"
     t.text "description"
     t.text "instructions"
-    t.string "servings"
-    t.string "integer"
     t.integer "cooking_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "servings"
   end
 
 end
